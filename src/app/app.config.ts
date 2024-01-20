32e0a37213d68cbd9ea4761742a788d77cf2045c
ageisler <geisler.gym@googlemail.com>
2024-01-20 22:30:12 +0100

    Added keycloak to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    KeycloakService
+  ]
 };
+
+export function initializeKeycloak(keycloak: KeycloakService): () => Promise<any> {
+  return () =>
+    keycloak.init({
+      config: {
+        url: ' https://keycloak.szut.dev/auth',
+        realm: 'szut',
+        clientId: 'employee-management-service-frontend',
+      },
+      initOptions: {
+        onLoad: 'check-sso', //login-required auch möglich
+        checkLoginIframe: false,
+      },
+      enableBearerInterceptor: true,
+    });
+}

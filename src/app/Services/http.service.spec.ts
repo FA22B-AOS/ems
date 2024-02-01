34d59ebf72568df3a472045efb761ec43ad2bdbd
AlphaDango <alpha.dango98@gmail.com>
2024-02-01 12:01:53 +0100

    Redirect von keycloak nun auf zuvor geklickte route

--- src/app/Services/http.service.spec.ts
+++ src/app/Services/http.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpService } from './Services/http.service';
+import { HttpService } from './http.service';
 
 describe('HTTPServiceService', () => {
   let service: HttpService;
